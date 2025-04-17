a80febbef6dd73d446cc62c711345d6749233c0d
zodang <1141ekdms@naver.com>
2025-04-17 22:15:17 +0900

    [Feat] MainScene UI 및 그리드 추가

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -14,6 +14,8 @@ public class GameManager : MonoBehaviour
     private GameObject currentPlayingStage;
     private OutputCircuit[] outputCircuits;
     public CameraController mainCamera;
+    private GridManager _grid;
+    private OneSceneUIManager _uiManager;
 
     private bool isGameOver = false;
     private float sayInterval = 60f;
@@ -29,7 +31,12 @@ public class GameManager : MonoBehaviour
     public gameState CurrentGameState
     {
         get => currentGameState;
-        set => currentGameState = value;
+        set
+        {
+            currentGameState = value;
+            _grid.ActivateGrid(currentGameState == gameState.GamePlaying);
+            _uiManager.ActivatePlayingCanvas(currentGameState == gameState.GamePlaying);
+        }
     }
     
     public GameObject CurrentPlayingStage
@@ -69,6 +76,10 @@ public class GameManager : MonoBehaviour
     {
         if (mainCamera == null)
             mainCamera = Camera.main.GetComponent<CameraController>();
+        
+        _grid = mainCamera.GetComponentInChildren<GridManager>();
+        _uiManager = FindAnyObjectByType<OneSceneUIManager>();
+        
         InitializeGame();
     }
 
