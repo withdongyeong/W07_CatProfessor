efabf5f8c68247d8f6e76bb06cf9db2084176e2b
je-ya <psychicartil@gmail.com>
2025-04-18 17:12:37 +0900

    [Add]중립회로 종류 추가

---
+++ Assets/Scripts/Shoot/ShootDirection/2WayUpRight.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "DirectionPattern2WayUpRight", menuName = "Mana System/Shoot Direction/2WayUpRight")]
+public class DirectionPattern2WayUpRight : ShootDirectionConfig
+{
+    public override Vector2[] GetShootDirections() => new[]
+    {
+        Vector2.up,
+        Vector2.right
+    };
+}
