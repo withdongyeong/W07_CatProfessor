05e1b58c4a78ae4dd021f0cd57a53c00722dac05
je-ya <psychicartil@gmail.com>
2025-04-21 00:38:29 +0900

    [Add] 커서 이미지 및 코드 추가

---
+++ Assets/YL/CustomCusor.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class CustomCursor2D : MonoBehaviour
+{
+    public Texture2D defaultCursor;
+    public Vector2 defaultHotspot = new Vector2(125, 125);
+
+
+
+    void Start()
+    {
+        Cursor.SetCursor(defaultCursor, defaultHotspot, CursorMode.Auto);
+    }
+
+
+}
